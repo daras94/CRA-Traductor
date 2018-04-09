9f9354c39c4e53626cd64804bc87f5156e21bbb9
Diego-Edgar <Oo.DEGP.oO@gmail.com>
2018-04-09 19:14:58 +0200

    Añadidas Palabras Diccionarios
    Nuevas palabras y cambios en los diccionarios

--- WorkSpaces/PECL-2 Analizador Sintactico/src/diccionario/dicES.pl
+++ WorkSpaces/PECL-2 Analizador Sintactico/src/diccionario/dicES.pl
@@ -1,12 +1,12 @@
-:- module(dicES,[
+﻿:- module(dicES,[
 
-	]).
+        ]).
 
 /*****************************************************************************************/
-/*       Dicionarios castellano basadose en la sintasis delejercicio 3.1 y 4.	     	 */
+/*       Dicionarios castellano basadose en la sintasis delejercicio 3.1 y 4.            */
 /*****************************************************************************************/
 /*****************************************************************************************/
-/*									Determinantes:										 */
+/*                                                                      Determinantes:                                                                           */
 /*****************************************************************************************/
 determinante(det(det_1), f, sg) --> [la]; [una].
 determinante(det(det_2), f, pl) --> [las]; [unas].
@@ -14,31 +14,31 @@ determinante(det(det_1), m, sg) --> [el]; [un].
 determinante(det(det_2), m, pl) --> [los]; [unos].
 
 /*****************************************************************************************/
-/*									Preposiciones:										 */
+/*                                                                      Preposiciones:                                                                           */
 /*****************************************************************************************/
-preposicion(prep(prep_1)) 	--> [a].
-preposicion(prep(prep_2)) 	--> [ante].
-preposicion(prep(prep_3)) 	--> [bajo].
-preposicion(prep(prep_4)) 	--> [con].
-preposicion(prep(prep_5)) 	--> [contra].
-preposicion(prep(prep_6)) 	--> [de].
-preposicion(prep(prep_7)) 	--> [desde].
-preposicion(prep(prep_8)) 	--> [en].
-preposicion(prep(prep_9)) 	--> [entre].
-preposicion(prep(prep_10)) 	--> [hacia].
-preposicion(prep(prep_11)) 	--> [hasta].
-preposicion(prep(prep_12)) 	--> [para].
-preposicion(prep(prep_13)) 	--> [por].
-preposicion(prep(prep_14)) 	--> [según].
-preposicion(prep(prep_15)) 	--> [sin].
-preposicion(prep(prep_16)) 	--> [so].
-preposicion(prep(prep_17)) 	--> [sobre].
-preposicion(prep(prep_18)) 	--> [tras].
-preposicion(prep(prep_19)) 	--> [durante].
-preposicion(prep(prep_20)) 	--> [mediante].
+preposicion(prep(prep_1))       --> [a].
+preposicion(prep(prep_2))       --> [ante].
+preposicion(prep(prep_3))       --> [bajo].
+preposicion(prep(prep_4))       --> [con].
+preposicion(prep(prep_5))       --> [contra].
+preposicion(prep(prep_6))       --> [de].
+preposicion(prep(prep_7))       --> [desde].
+preposicion(prep(prep_8))       --> [en].
+preposicion(prep(prep_9))       --> [entre].
+preposicion(prep(prep_10))      --> [hacia].
+preposicion(prep(prep_11))      --> [hasta].
+preposicion(prep(prep_12))      --> [para].
+preposicion(prep(prep_13))      --> [por].
+preposicion(prep(prep_14))      --> [según].
+preposicion(prep(prep_15))      --> [sin].
+preposicion(prep(prep_16))      --> [so].
+preposicion(prep(prep_17))      --> [sobre].
+preposicion(prep(prep_18))      --> [tras].
+preposicion(prep(prep_19))      --> [durante].
+preposicion(prep(prep_20))      --> [mediante].
 
 /*****************************************************************************************/
-/*										Nombres:										 */
+/*                                                                              Nombres:                                                                                 */
 /*****************************************************************************************/
 % Los Singulares y los singulares.
 nombre(n(n_0), f, sg)  --> [piedra].
@@ -67,7 +67,7 @@ nombre(n(n_13), m, sg) --> [universidad].
                                  
 
 /*****************************************************************************************/
-/*										Verbos: 										 */
+/*                                                                              Verbos:                                                                                  */
 /*****************************************************************************************/
 % Los Singulares.
 verbo(v(v_1), sg) --> [corta].
@@ -90,7 +90,7 @@ verbo(v(v_7), pl) --> [cazan].
 verbo(v(v_8), pl) --> [son].
                           
 /*****************************************************************************************/
-/*										Adjetivos:										 */
+/*                                                                              Adjetivos:                                                                               */
 /*****************************************************************************************/                        
 % Los Singulares Invariables.
 adjetivo(adj(adj_1), sg) --> [roja].
@@ -98,33 +98,44 @@ adjetivo(adj(adj_2), sg) --> [negro].
 adjetivo(adj(adj_4), sg) --> [gris].
 adjetivo(adj(adj_3), sg) --> [grande].
 adjetivo(adj(adj_5), sg) --> [pequeño].
-                                
+adjetivo(adj(adj_6), sg) --> [delgado].
+adjetivo(adj(adj_7), sg) --> [alta].
+
+%Posesivos
+adjetivo(adj(adj_8), sg) --> [mi].
 
 /*****************************************************************************************/
-/*										Pronombres:										 */
-/*****************************************************************************************/                               
-pronombre   	--> [yo]; [tú]; [él], [ella], [nosotros]; [nosotras]; [vosotros]; [vosotras]; [ellos]; [ellas];
-                    [éste]; [ésta]; [esto]; [éstos]; [éstas]; [ése]; [ésa]; [éso]; [esos]; [ésa];
-                    [aquél]; [aquélla]; [aquello]; [aquéllos]; [aquéllas];
-                    [mío]; [mía]; [míos]; [mías]; ([tuy], ([o]; [a]; [os]; [as])); ([suy], ([o]; [a]; [os]; [as])).
+/*                                                                              Pronombres:                                                                              */
+/*****************************************************************************************/
+% Personales
+pronombre(pron(pron_1), sg) --> [yo].
+pronombre(pron(pron_2), sg) --> [tú].
+pronombre(pron(pron_3), sg) --> [él].
+pronombre(pron(pron_4), sg) --> [ella].
+pronombre(pron(pron_5), sg) --> [ello].
+pronombre(pron(pron_6), pl) --> [nosotros].
+pronombre(pron(pron_7), pl) --> [vosotros].
+pronombre(pron(pron_8), pl) --> [ellos].
+                    
+%relativo
+pronombre(pron(pron_9))   --> [que].
 
 /*****************************************************************************************/
-/*										Conjunciones:									 */
+/*                                                                              Conjunciones:                                                                    */
 /*****************************************************************************************/
 % Subordinadas
-conjuncion_sub(conj_s(conj_s1))   --> [que].
 
 % Cordinadas.
 conjuncion_coord(conj_c(conj_c1)) --> [y].
 
 /*****************************************************************************************/
-/*										Adverbios:										 */
+/*                                                                              Adverbios:                                                                               */
 /*****************************************************************************************/
 adverbio(adv(adv_1)) --> [muy].
 adverbio(adv(adv_2)) --> [bien].
 
 /*****************************************************************************************/
-/*										Articulos:										 */
+/*                                                                              Articulos:                                                                               */
 /*****************************************************************************************/
 % Los Singulares.
 articulo(m(art), f, sg) --> [la].
--- WorkSpaces/PECL-2 Analizador Sintactico/src/diccionario/dicIN.pl
+++ WorkSpaces/PECL-2 Analizador Sintactico/src/diccionario/dicIN.pl
@@ -1,97 +1,95 @@
-:- module(dicIN,[
-		
-	]).
+﻿:- module(dicIN,[
+                
+        ]).
 
 
 /*****************************************************************************************/
-/*       Dicionarios castellano basadose en la sintasis delejercicio 3.1 y 4.	     	 */
+/*       Dicionarios castellano basadose en la sintasis delejercicio 3.1 y 4.            */
 /*****************************************************************************************/
 /*****************************************************************************************/
-/*									modificadores:										 */
+/*                                                                      modificadores:                                                                           */
 /*****************************************************************************************/
-modifier(m(art))		--> [the].
-modifier(det(det_1)) 	--> [the]; [a]; [an].
-modifier(det(det_2)) 	--> [the]; [some].
-modifier(det(det_1)) 	--> [the]; [a]; [an].
-modifier(det(det_2)) 	--> [the]; [some].
+modifier(m(art))                --> [the].
+modifier(det(det_1))    --> [the]; [a]; [an].
+modifier(det(det_2))    --> [the]; [some].
+modifier(det(det_1))    --> [the]; [a]; [an].
+modifier(det(det_2))    --> [the]; [some].
 
 
 /*****************************************************************************************/
-/*									prepositionses:										 */
+/*                                                                      prepositionses:                                                                          */
 /*****************************************************************************************/
-prepositions(prep(prep_1)) 		--> [to].
-prepositions(prep(prep_2)) 		--> [before].
-prepositions(prep(prep_3)) 		--> [under].
-prepositions(prep(prep_4)) 		--> [with]; [by]; [cum].
-prepositions(prep(prep_5)) 		--> [to].
-prepositions(prep(prep_6)) 		--> [from].
-prepositions(prep(prep_7)) 		--> [since].
-prepositions(prep(prep_8)) 		--> [in].
-prepositions(prep(prep_9)) 		--> [between].
-prepositions(prep(prep_10)) 	--> [toward].
-prepositions(prep(prep_11)) 	--> [until].
-prepositions(prep(prep_12)) 	--> [for].
-prepositions(prep(prep_13)) 	--> [by].
-prepositions(prep(prep_14)) 	--> [according].
-prepositions(prep(prep_15)) 	--> [without].
-prepositions(prep(prep_16)) 	--> [so].
-prepositions(prep(prep_17)) 	--> [on].
-prepositions(prep(prep_18)) 	--> [after].
-prepositions(prep(prep_19)) 	--> [during].
-prepositions(prep(prep_20)) 	--> [through].
+prepositions(prep(prep_1))              --> [to].
+prepositions(prep(prep_2))              --> [before].
+prepositions(prep(prep_3))              --> [under].
+prepositions(prep(prep_4))              --> [with]; [by]; [cum].
+prepositions(prep(prep_5))              --> [to].
+prepositions(prep(prep_6))              --> [from].
+prepositions(prep(prep_7))              --> [since].
+prepositions(prep(prep_8))              --> [in].
+prepositions(prep(prep_9))              --> [between].
+prepositions(prep(prep_10))     --> [toward].
+prepositions(prep(prep_11))     --> [until].
+prepositions(prep(prep_12))     --> [for].
+prepositions(prep(prep_13))     --> [by].
+prepositions(prep(prep_14))     --> [according].
+prepositions(prep(prep_15))     --> [without].
+prepositions(prep(prep_16))     --> [so].
+prepositions(prep(prep_17))     --> [on].
+prepositions(prep(prep_18))     --> [after].
+prepositions(prep(prep_19))     --> [during].
+prepositions(prep(prep_20))     --> [through].
 
 /*****************************************************************************************/
-/*										nouns:										 */
+/*                                                                              nouns:                                                                           */
 /*****************************************************************************************/
 % Los plurales y los Singulares.
-noun(n(n_0), sg)		--> [stone].
-noun(n(n_1), sg)		--> [paper].
-noun(n(n_2), pl)		--> [scissors].
-noun(n(n_3), sg)  		--> [man].        
-noun(n(n_3), pl)  		--> [menfolk].
-noun(n(n_4), sg)  		--> [woman].
-noun(n(n_4), pl)  		--> [women].
-noun(n(n_5), sg)  		--> [juan].
-noun(n(n_6), sg)  		--> [maría].
-noun(n(n_7), sg)  		--> [apple].
-noun(n(n_7), pl)  		--> [apples].
-noun(n(n_8), sg)  		--> [cat].
-noun(n(n_8), pl)  		--> [cats].
-noun(n(n_9), sg)  		--> [cat].
-noun(n(n_9), pl)  		--> [gatas].
-noun(n(n_10), sg) 		--> [mouse].
-noun(n(n_10), pl) 		--> [mice].
-noun(n(n_11), sg) 		--> [schoolchild].
-noun(n(n_11), pl) 		--> [students].
-noun(n(n_12), sg) 		--> [schoolgirl].
-noun(n(n_12), pl) 		--> [students].
-noun(n(n_13), sg) 		--> [university].
+noun(n(n_0), sg)                --> [stone].
+noun(n(n_1), sg)                --> [paper].
+noun(n(n_2), pl)                --> [scissors].
+noun(n(n_3), sg)                --> [man].        
+noun(n(n_3), pl)                --> [menfolk].
+noun(n(n_4), sg)                --> [woman].
+noun(n(n_4), pl)                --> [women].
+noun(n(n_5), sg)                --> [juan].
+noun(n(n_6), sg)                --> [mary].
+noun(n(n_7), sg)                --> [apple].
+noun(n(n_7), pl)                --> [apples].
+noun(n(n_8), sg)                --> [cat].
+noun(n(n_8), pl)                --> [cats].
+noun(n(n_9), sg)                --> [cat].
+noun(n(n_9), pl)                --> [gatas].
+noun(n(n_10), sg)               --> [mouse].
+noun(n(n_10), pl)               --> [mice].
+noun(n(n_11), sg)               --> [schoolchild].
+noun(n(n_11), pl)               --> [students].
+noun(n(n_12), sg)               --> [schoolgirl].
+noun(n(n_12), pl)               --> [students].
+noun(n(n_13), sg)               --> [university].
 
 /*****************************************************************************************/
-/*										verbs: 										     */
+/*                                                                              verbs:                                                                               */
 /*****************************************************************************************/
 % Los Singulares
-verb(v(v_1), sg)		--> [cuts].
-verb(v(v_2), sg)		--> [wraps].
-verb(v(v_3), sg)		--> [breaks].
-verb(v(v_4), sg) 		--> [loves].
-verb(v(v_5), sg) 		--> [was].
-verb(v(v_6), sg) 		--> [eat].
-verb(v(v_7), sg) 		--> [saucepan].
-verb(v(v_8), sg) 		--> [is].
+verb(v(v_1), sg)                --> [cuts].
+verb(v(v_2), sg)                --> [wraps].
+verb(v(v_3), sg)                --> [breaks].
+verb(v(v_4), sg)                --> [loves].
+verb(v(v_5), sg)                --> [was].
+verb(v(v_6), sg)                --> [eats].
+verb(v(v_7), sg)                --> [is].
 
 % Los Plurales.
-verb(v(v_1), pl)		--> [cut].
-verb(v(v_2), pl)		--> [wrap].
-verb(v(v_3), pl)		--> [break].
-verb(v(v_4), pl) 		--> [love].
-verb(v(v_5), pl) 		--> [were].
-verb(v(v_6), pl) 		--> [eat].
-verb(v(v_7), pl) 		--> [hunt].
-verb(v(v_8), pl) 		--> [are].
+verb(v(v_1), pl)                --> [cut].
+verb(v(v_2), pl)                --> [wrap].
+verb(v(v_3), pl)                --> [break].
+verb(v(v_4), pl)                --> [love].
+verb(v(v_5), pl)                --> [were].
+verb(v(v_6), pl)                --> [eat].
+verb(v(v_7), pl)                --> [are].
 
 /*****************************************************************************************/
-/*										adjectives:										 */
+/*                                                                              adjectives:                                                                              */
 /*****************************************************************************************/                        
 % Los Singulares Invariables.
 adjective(adj(adj_1), sg) --> [red].
@@ -99,19 +97,34 @@ adjective(adj(adj_2), sg) --> [black].
 adjective(adj(adj_4), sg) --> [gray].
 adjective(adj(adj_3), sg) --> [big].
 adjective(adj(adj_5), sg) --> [little].
-
+adjective(adj(adj_6), sg) --> [thin].
+adjective(adj(adj_7), sg) --> [tall].
+% Possessive
+adjective(adj(adj_8), sg) --> [my].
 
 /*****************************************************************************************/
-/*										Conjunctions:									 */
+/*                                                                              pronouns:                                                                              */
 /*****************************************************************************************/
-% Subordinadas
-conjunctions_sub(conj_s(conj_s1))   --> [that].
+pronoun(pron(pron_1), sg) --> [i].
+pronoun(pron(pron_2), sg) --> [you].
+pronoun(pron(pron_3), sg) --> [he].
+pronoun(pron(pron_4), sg) --> [she].
+pronoun(pron(pron_5), sg) --> [it].
+pronoun(pron(pron_6), pl) --> [we].
+pronoun(pron(pron_7), pl) --> [you].
+pronoun(pron(pron_8), pl) --> [they].
+% Relativo
+pronoun(pron(pron_9)) --> [that].
+
+/*****************************************************************************************/
+/*                                                                              Conjunctions:                                                                    */
+/*****************************************************************************************/
 
 % Cordinadas.
 conjunctions_coord(conj_c(conj_c1)) --> [and].
 
 /*****************************************************************************************/
-/*										adverbs:										 */
+/*                                                                              adverbs:                                                                                 */
 /*****************************************************************************************/
 adverb(adv(adv_1)) --> [very].
 adverb(adv(adv_2)) --> [good].
